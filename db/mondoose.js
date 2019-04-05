25118412203686d887cd85abfcf2c8d206ae3fe9
Илья <ila@MacBook-Pro-Ila.local>
2019-04-05 12:31:14 +0300

    first comiit

--- db/mondoose.js
+++ db/mondoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/ChatAPI');
+mongoose.connect('mongodb://testuser:testuser1@ds133086.mlab.com:33086/heroku_583d2fr5');
 
 module.exports = { mongoose };
